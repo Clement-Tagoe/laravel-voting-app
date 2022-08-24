019daa3fd41695a637993ac25fed872d8b1e4d64
Clement-Tagoe <clementtagoe110@gmail.com>
2022-08-24 12:40:47 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.69rem', {lineHeight: '1rem'}],
+            },
+            spacing: {
+                22: '5.5rem',
+                70: '17.5rem',
+                74: '18.75rem',
+                76: '19rem',
+                104: '26rem',
+                175: '43.75rem',
+                
+            }
+        },
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
+};
